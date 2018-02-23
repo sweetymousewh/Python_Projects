9601b70e854f0d0197952ea296888f3a84ec7400
sweetymousewh <jack.w0529@gmail.com>
2018-02-24 00:04:15 +1100

    Just draft

--- scanner1.0.py
+++ scanner1.0.py
@@ -1,6 +1,15 @@
 import socket
 
 
+# scan existing opening ports used on localhost
+
+# print the result of that
+
+# store the result to a new file
+
+# if there is sth wrong write out the error report
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = 'google.com.au'
 
